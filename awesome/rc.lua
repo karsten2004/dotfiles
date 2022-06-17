e64ac525831b170f9527f15fb16db218d18afd60
karsten <karsten@vanderdeijl.net>
2022-06-17 12:02:58 +0200

    trying theming it's blood weird

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -90,7 +90,7 @@ awful.layout.layouts = {
 
 lock_screen = function ()
 --    awful.util.spawn("i3lock --no-verify --pass-media-keys --pass-volume-keys --bar-indicator -k -f --image='/home/karsten/Pictures/Backgrounds/Astronaut dude.png'")
-    awful.util.spawn("i3lock --pass-media-keys --pass-volume-keys --bar-indicator -k -f --image='/home/karsten/Pictures/Backgrounds/Astronaut dude.png'")
+    awful.util.spawn("i3lock --ignore-empty-password --pass-media-keys --pass-volume-keys --force-clock --time-size=36 --date-size=18 --time-pos=\"w/2:iy+4\" --date-pos=\"tx:ty+24\" --indicator --radius 32 --ind-pos=\"(w/2)-130:h/2\" --wrong-text=\"\" --verif-text=\"\" --noinput-text=\"\" --lock-text=\"\" --lockfailed-text=\"screen not locked\" --time-color=FDFCFFFF --date-color=FDFCFFFF --ring-width 7 --inside-color=16143B7F --ring-color=16143BFF --keyhl-color=689F4CFF --bshl-color=5D1231FF --separator-color=16143B7F --insidewrong-color=5D12317F --ringwrong-color=5D1231FF --ringver-color=77DEF1FF --insidever-color=77DEF17F --line-uses-inside --no-modkey-text --image=\'/home/karsten/Pictures/Backgrounds/Astronaut dude.png\'")
 end
 
 -- }}}

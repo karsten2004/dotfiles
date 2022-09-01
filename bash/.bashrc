d3051730926afe687da4e1d3a6a1afb105b8579f
karsten <karsten@vanderdeijl.net>
2022-09-01 21:38:05 +0200

    removed custom stow alias because it was dumb and is no longer needed

--- bash/.bashrc
+++ bash/.bashrc
@@ -1,2 +1,1 @@
 alias "lock"="$HOME/dotfiles/i3lock-color/i3lock-color.sh"
-alias "dotfiles-stow"="$HOME/dotfiles/stow/dotfiles-stow.sh"
--- stow/dotfiles-stow.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-targetDir="$HOME/$1"
-mkdir $targetDir &> /dev/null
-mkdir $targetDir/$3 &> /dev/null
-#echo "stow --dir=/home/karsten/dotfiles/ --target=$targetDir/$3 $2 $3"
-stow --dir=/home/karsten/dotfiles/ --target=$targetDir/$3 $2 $3
